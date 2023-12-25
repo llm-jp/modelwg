aef611139d89927bab4339a239f329a769b2be24
Hiroshi Matsuda <hiroshi_matsuda@megagon.ai>
2023-12-26 03:39:49 +0900

    refactor

--- upload_to_hf.py
+++ upload_to_hf.py
@@ -1,56 +1,50 @@
-# Copyright (c) 2021, EleutherAI
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import os
 import sys
 
 from huggingface_hub import HfApi, create_repo
 
-converted_ckpt = sys.argv[1]
-repo_name = sys.argv[2]
-branch_name = sys.argv[3]
-try:
-    create_repo(repo_name, repo_type="model", private=True)
-except:
-    print("repo {repo_name} already exists!")
-    pass
-
-files = os.listdir(converted_ckpt)
-
-api = HfApi()
-if branch_name != "main":
+def main():
+    converted_ckpt = sys.argv[1]
+    repo_name = sys.argv[2]
+    branch_name = sys.argv[3]
     try:
-        api.create_branch(
+        create_repo(repo_name, repo_type="model", private=True)
+    except:
+        print("repo {repo_name} already exists and will be upload target.")
+        pass
+
+    api = HfApi()
+    if branch_name != "main":
+        try:
+            api.create_branch(
+                repo_id=repo_name,
+                repo_type="model",
+                branch=branch_name,
+            )
+        except:
+            print(f"branch {branch_name} already exists, try again...")
+            exit(1)
+
+    print(f"to upload: {converted_ckpt}")
+    uploaded_count = 0
+    for file in os.listdir(converted_ckpt):
+        path_or_file = os.path.join(converted_ckpt, file)
+        if not os.path.isfile(path_or_file):
+            print(f"skipping {file} ...")
+            continue
+        print(f"uploading {file} to branch {branch_name} ...")
+        api.upload_file(
+            path_or_fileobj=path_or_file,
+            path_in_repo=file,
             repo_id=repo_name,
             repo_type="model",
-            branch=branch_name,
+            commit_message=f"Upload {file}",
+            revision=branch_name,
         )
-    except:
-        print(f"branch {branch_name} already exists, try again...")
-print(f"to upload: {files}")
-for file in files:
-    path_or_file = os.path.join(converted_ckpt, file)
-    # checkpointなどのディレクトリを回避
-    if not os.path.isfile(path_or_file):
-        continue
-    print(f"Uploading {file} to branch {branch_name}...")
-    api.upload_file(
-        path_or_fileobj=path_or_file,
-        path_in_repo=file,
-        repo_id=repo_name,
-        repo_type="model",
-        commit_message=f"Upload {file}",
-        revision=branch_name,
-    )
-    print(f"Successfully uploaded {file} !")
+        print(f"successfully uploaded {file}")
+        uploaded_count += 1
+    print(f"{uploaded_count} files were uploaded to {repo_name} {branch_name}")
+
+
+if __name__ == "__main__":
+    main()
