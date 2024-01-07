e3afa19528a1d03418eafc8b9e5834c1f2e63db9
Hiroshi Matsuda <hiroshi_matsuda@megagon.ai>
2024-01-07 16:30:44 +0900

    improve messages

--- upload_to_hf.py
+++ upload_to_hf.py
@@ -6,6 +6,9 @@ from huggingface_hub import HfApi, create_repo
 from huggingface_hub.utils._errors import BadRequestError, HfHubHTTPError
 
 
+TODO_COMMENT = "**TODO: Add base_model description to model card section in Hugging Face Hub**"
+
+
 def main():
     converted_ckpt = sys.argv[1]
     repo_name = sys.argv[2]
@@ -35,13 +38,14 @@ def main():
 
     print(f"to upload: {converted_ckpt}")
     uploaded_count = 0
+    temp_path = None
+    base_model_line = None
     for file in os.listdir(converted_ckpt):
         path_or_file = os.path.join(converted_ckpt, file)
         if not os.path.isfile(path_or_file):
             print(f"skipping {file} ...")
             continue
         print(f"uploading {file} to branch {branch_name} ...")
-        retry = False
         while True:
             try:
                 api.upload_file(
@@ -56,7 +60,7 @@ def main():
                 uploaded_count += 1
                 break
             except BadRequestError as e:
-                if file == "README.md":
+                if not temp_path and file == "README.md":
                     temp_path, base_model_line = copy_with_base_model_filter(path_or_file)
                     if temp_path:
                         print(f"{base_model_line} does not exist in Hugging Face hub. Retrying upload README.md with the base_model line removed.", file=sys.stderr)
@@ -64,6 +68,10 @@ def main():
                         continue
                 raise e
     print(f"{uploaded_count} files were uploaded to {repo_name} {branch_name}")
+    if temp_path:
+        print(f"{base_model_line} does not exist in Hugging Face hub.")
+        print(TODO_COMMENT)
+        os.remove(temp_path)
 
 
 def copy_with_base_model_filter(path):
@@ -82,12 +90,10 @@ def copy_with_base_model_filter(path):
                 elif in_model_card:
                     in_model_card = False
                     if base_model_line:
-                        lines.append(line)
-                        lines.append("**TODO: Add base_model description in model card section**")
-                        lines.append("")
-            lines.append(_)
+                        line += "\n" + TODO_COMMENT
+            lines.append(line)
     if base_model_line:
-        with tempfile.NamedTemporaryFile(mode="w", delete_on_close=False) as fout:
+        with tempfile.NamedTemporaryFile(mode="w", delete=False) as fout:
             print(*lines, sep="\n", file=fout)
         return fout.name, base_model_line
     else:
