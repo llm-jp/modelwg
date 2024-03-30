0d311ac6fea4a69e4484124f447afc303a0360cf
Sosuke Hosokawa <47357860+so298@users.noreply.github.com>
2024-03-30 11:15:55 +0900

    Add exp9 scripts

---
+++ sft-scripts/exp9_gpt4-self-inst_full.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Single node full parameter sft script
+
+set -eux
+
+# Move to script path
+SCRIPT_PATH="llm-jp-sft/"
+cd $SCRIPT_PATH
+source venv/bin/activate
+
+# Target Model (TODO: Change if needed)
+TARGET_MODEL="/model/7B_HF/llm-jp-7b-CC_v2_100k_ver3.0" # exp9
+SFT_TYPE="full-gpt4-self-inst"
+OUTPUT_DIR="/model/7B_HF_RE/$(basename ${TARGET_MODEL%/})_${SFT_TYPE}"
+
+# wandb info
+export WANDB_ENTITY="llm-jp"
+export WANDB_PROJECT="7b-sft-and-eval"
+export WANDB_NAME="sft-exp9-${SFT_TYPE}"
+
+# For debugging
+export NCCL_DEBUG=WARN
+
+# Training settings
+config_file="configs/accelerate_config_zero3.yaml"
+dataset_path="./dataset"
+dataset_sh="./mdx/dataset_gpt4_self_inst_ja.sh"
+num_train_epochs=5
+per_device_train_batch_size=1
+gradient_accumulation_steps=8  # global_batch_size = per_device_train_batch_size * n_gpus * gradient_accumulation_steps = 64
+max_seq_length=4096             # for llama model
+learning_rate="2e-5"
+lr_scheduler_type="cosine"
+warmup_ratio=0.1
+
+# N_gpu = 8
+accelerate launch --config_file $config_file \
+    train.py \
+    --model_name_or_path $TARGET_MODEL \
+    --tokenizer_name_or_path $TARGET_MODEL \
+    --num_train_epochs $num_train_epochs \
+    --per_device_train_batch_size $per_device_train_batch_size \
+    --gradient_accumulation_steps $gradient_accumulation_steps \
+    --learning_rate $learning_rate \
+    --warmup_ratio $warmup_ratio \
+    --lr_scheduler_type $lr_scheduler_type \
+    --bf16 \
+    --max_seq_length $max_seq_length \
+    --logging_steps 10 \
+    --report_to wandb \
+    --data_files $($dataset_sh $dataset_path) \
+    --output_dir "$OUTPUT_DIR"
